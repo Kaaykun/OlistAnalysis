d688645a0e84ecb345826a2a8c301f38452b9f36
Jaris Fenner <jaris.a.fenner@gmail.com>
2024-02-14 14:48:05 +0900

    Fixed spelling error, removed unused .py file

--- utils/data.py
+++ utils/data.py
@@ -6,8 +6,8 @@ class Olist:
     def get_data(self):
         """
         This function returns a Python dict.
-        Its keys should be 'sellers', 'orders', 'order_items' etc...
-        Its values should be pandas.DataFrames loaded from csv files
+        Its keys are 'sellers', 'orders', 'order_items' etc...
+        Its values are pandas.DataFrames loaded from csv files
         """
         csv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'data/csv')
         file_names = [file for file in os.listdir(csv_path) if file.endswith('.csv')]
